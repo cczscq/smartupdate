04d5adbdb83115a50cd15973bc915552cc4c4db6
zhaosc <cczscq_2010@gmail.com>
2014-01-04 14:11:57 +0800

    Initial Commit

---
+++ SmartUpdate/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  SmartUpdate
+//
+//  Created by zhaosc  on 1/4/14.
+//  Copyright (c) 2014 zhaosc . All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
